ac5400a2391f5efb79a8c93a7ce68cd56802a9a3
lovesnow <houshao55@gmail.com>
2013-11-22 10:31:13 +0800

    update models

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from PyQt5 import QtCore
 
 import requests
 import utils
+import xdg
 
 from xmltodict import parse as xml_parse
 
@@ -96,7 +97,7 @@ class SuggestModel(QtCore.QAbstractListModel):
         else:    
             self.resetSuggestData()
     
-    def emitSuggesResult(self, data, threadId):        
+    def emitSuggestResult(self, data, threadId):        
         if threadId == self.suggestThreadId:
             self.suggested.emit(threadId, data)
             
@@ -109,7 +110,7 @@ class SuggestModel(QtCore.QAbstractListModel):
         thread_id = copy.deepcopy(self.suggestThreadId)
         utils.ThreadFetch(
             fetch_funcs=(suggestFunc, args),
-            success_funcs=(self.emitSuggesResult, (thread_id,))).start()
+            success_funcs=(self.emitSuggestResult, (thread_id,))).start()
         
     @QtCore.pyqtSlot(str)
     def suggest(self, text):    
@@ -120,3 +121,118 @@ class SuggestModel(QtCore.QAbstractListModel):
         self.asyncSuggest(get_suggest, (text, num))
     
 suggestModel = SuggestModel()        
+
+
+class KeyDict(dict):
+    
+    def __eq__(self, other):
+        try:
+            return self.owner == other.owner
+        except:
+            return False
+        
+    @property    
+    def owner(self):
+        return self.get("title")
+        
+    def __cmp__(self, other):    
+        try:
+            return cmp(self.owner, other.owner)
+        except:
+            return -1
+            
+
+class HistoryModel(QtCore.QAbstractListModel):
+    TitleRole = QtCore.Qt.UserRole + 1
+    ExplainRole = QtCore.Qt.UserRole + 2
+    
+    _roles = { TitleRole: "title", ExplainRole: "explain" }
+    
+    finished = QtCore.pyqtSignal()
+    
+    def __init__(self, parent=None):
+        super(HistoryModel, self).__init__(parent)
+        self._db = xdg.get_cache_file("history.db")
+        self._data = []
+        
+        # load db
+        self.load()
+        
+    def load(self):    
+        objs = utils.load_db(self._db)
+        if objs:
+            self._data = objs
+    
+    @QtCore.pyqtSlot()        
+    def save(self):
+        if self._data:
+            utils.save_db(self._data, self._db)
+        
+    def setHistoryData(self, data):
+        self.beginResetModel()
+        del self._data
+        self._data = data        
+        self.endResetModel()  
+        self.finished.emit()
+        
+    def resetHistoryData(self):    
+        self.beginResetModel()
+        self.endResetModel()
+        self.finished.emit()
+                    
+    def addHistoryData(self, history):
+        self.beginInsertRows(QtCore.QModelIndex(), self.rowCount(), self.rowCount())
+        self._data.append(history)
+        self.endInsertRows()        
+        
+    @QtCore.pyqtSlot(str, str)
+    def addSearchData(self, title, explain):
+        title = title.strip()
+        explain = explain.split("<br>")[0]
+        kd = KeyDict(title=title, explain=explain)
+        change = False
+        if kd in self._data:
+            idx = self._data.index(kd)
+            if idx != 0:
+                self._data.insert(0, self._data.pop(idx))
+                change = True
+        else:    
+            self._data.insert(0, kd)
+            change = True
+            
+        if change:    
+            self.resetHistoryData()
+            self.save()
+            
+    @QtCore.pyqtSlot(result=int)
+    def total(self):    
+        return len(self._data)
+        
+    def removeHistoryData(self):
+        self.beginInsertRows(QtCore.QModelIndex(), self.rowCount(), self.rowCount())
+        self._data.pop()        
+        self.endInsertRows()        
+        
+    def roleNames(self):
+        return self._roles
+        
+    def rowCount(self, parent=QtCore.QModelIndex()):    
+        return len(self._data[:10])
+    
+    def data(self, index, role):
+
+        if not index.isValid() or index.row() > len(self._data):
+            return QtCore.QVariant()
+
+        try:
+            item = self._data[index.row()]
+        except:
+            return QtCore.QVariant()
+        
+        if role == self.TitleRole:
+            return item.get("title", "")
+        elif role == self.ExplainRole:
+            return item.get("explain", "")
+        return QtCore.QVariant()
+
+historyModel = HistoryModel()        
