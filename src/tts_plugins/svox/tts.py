d153ada0d69e780dd8f8a1f4cf15115eee449ec2
Andy Stewart <lazycat.manatee@gmail.com>
2013-12-15 13:12:10 +0800

    Finish svox tts engine.

---
+++ src/tts_plugins/svox/tts.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2013 Deepin, Inc.
+#               2011 ~ 2013 Wang Yong
+# 
+# Author:     Wang Yong <lazycat.manatee@gmail.com>
+# Maintainer: Wang Yong <lazycat.manatee@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+
+def get_voice(text):
+    wav_file = "/tmp/deepin-translator-svox.wav"
+    subprocess.Popen('''pico2wave -w %s %s''' % (wav_file, text), shell=True).wait()
+    return wav_file
