e396d2243954ed9d6aa50018d3f6171af9dc88be
Andy Stewart <lazycat.manatee@gmail.com>
2013-12-16 00:14:06 +0800

    Add priority for tts plugins.

--- src/tts_plugin.py
+++ src/tts_plugin.py
@@ -50,7 +50,10 @@ class TtsPlugin(QObject):
         if self.local_dict.has_key(src_lang):
             engine_list += self.local_dict[src_lang]
             
-        return engine_list + self.network_all_list + self.local_all_list    
+        voice_list = engine_list + self.network_all_list + self.local_all_list
+        voice_list = sorted(voice_list, key=lambda (name, display_name, priority): priority, reverse=True)
+        
+        return map(lambda (name, display_name, priority): (name, display_name), voice_list)
     
     @pyqtSlot(str, result="QVariant")
     def get_voice_model(self, src_lang):
@@ -74,6 +77,7 @@ class TtsPlugin(QObject):
             need_network = is_true(plugin_config.get("Voice Info", "need_network"))
             support_all_language = is_true(plugin_config.get("Voice Info", "support_all_language"))
             support_languages_info = plugin_config.get("Voice Info", "support_languages")
+            priority = plugin_config.get("Voice Info", "priority")
             if support_languages_info == None:
                 support_languages = []
             else:
@@ -81,14 +85,14 @@ class TtsPlugin(QObject):
                 
             if need_network:
                 if support_all_language:
-                    self.network_all_list.append((plugin_name, plugin_display_name))
+                    self.network_all_list.append((plugin_name, plugin_display_name, priority))
                 else:
-                    self.update_dict(self.network_dict, support_languages, plugin_name, plugin_display_name)
+                    self.update_dict(self.network_dict, support_languages, plugin_name, plugin_display_name, priority)
             else:
                 if support_all_language:
-                    self.local_all_list.append((plugin_name, plugin_display_name))
+                    self.local_all_list.append((plugin_name, plugin_display_name, priority))
                 else:
-                    self.update_dict(self.local_dict, support_languages, plugin_name, plugin_display_name)
+                    self.update_dict(self.local_dict, support_languages, plugin_name, plugin_display_name, priority)
 
         # import pprint            
         # pp = pprint.PrettyPrinter()
@@ -97,10 +101,10 @@ class TtsPlugin(QObject):
         # pp.pprint(self.network_dict)
         # pp.pprint(self.local_dict)
 
-    def update_dict(self, info_dict, languages, plugin_name, plugin_display_name):
+    def update_dict(self, info_dict, languages, plugin_name, plugin_display_name, priority):
         for language in languages:
             if not info_dict.has_key(language):
                 info_dict[language] = []
                 
             info_list = info_dict[language]    
-            info_list.append((plugin_name, plugin_display_name))
+            info_list.append((plugin_name, plugin_display_name, priority))
